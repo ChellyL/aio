aba68c598f7ddcf8b4b6ed73289ab835bbfded09
ChellyL <38887991+ChellyL@users.noreply.github.com>
2023-04-16 19:49:14 +0800

    Update aio.sh

--- aio.sh
+++ aio.sh
@@ -46,20 +46,11 @@ media(){
 
 #6 warp设置
 warp(){
-    bash <(curl -fsSL https://cdn.jsdelivr.net/gh/missuo/CloudflareWarp/warp.sh)
+    #bash <(curl -fsSL https://cdn.jsdelivr.net/gh/missuo/CloudflareWarp/warp.sh)
+    bash <(curl https://raw.githubusercontent.com/fscarmen/warp/main/menu.sh)
 }
 
-#7 安装SS
-ss(){
-    bash <(curl -s -L https://raw.githubusercontent.com/loyess/Shell/master/ss-plugins.sh)
-}
-
-#8 安装证书
-acme(){
-    wget -N --no-check-certificate https://raw.githubusercontent.com/Misaka-blog/acme-script/main/acme.sh && bash acme.sh
-}
-
-#9 改root登录
+#7 改root登录
 login(){
     echo "--------------------------------------------------------------"
     echo "输入以下代码："
@@ -72,79 +63,95 @@ login(){
     echo "--------------------------------------------------------------"
 }
 
-#10 安装歇斯底里
-hysteria(){
-    bash <(curl -fsSL https://git.io/hysteria.sh)
+#8 安装证书
+acme(){
+    wget -N --no-check-certificate https://raw.githubusercontent.com/Misaka-blog/acme-script/main/acme.sh && bash acme.sh
 }
 
-#11 安装naiveproxy
-naive(){
-        bash <(curl -s -L https://raw.githubusercontent.com/ChellyL/aio/main/naiveproxy.sh)
+#8 安装SS
+ss(){
+    bash <(curl -s -L https://raw.githubusercontent.com/loyess/Shell/master/ss-plugins.sh)
 }
 
-#12 安装naiveproxy yg原版
-naive_yg(){
-        wget -N https://gitlab.com/rwkgyg/naiveproxy-yg/raw/main/naiveproxy.sh && bash naiveproxy.sh
-}
-
-#13 安装v2ray
-V2Ray(){ 
-    bash <(curl -s -L https://raw.githubusercontent.com/ChellyL/aio/main/v2.sh)
-}
-
-#14 安装v2ray mack-a原版
-V2Ray_ma(){ 
-    wget -P /root -N --no-check-certificate "https://raw.githubusercontent.com/mack-a/v2ray-agent/master/install.sh" && chmod 700 /root/install.sh && /root/install.sh
-}
-
-#15 安装Tg专用代理（Go版）
+#9 安装Tg专用代理（Go版）
 Tg_go(){
     bash <(curl -s -L wget https://raw.githubusercontent.com/ChellyL/Docker-mtproto/main/go_mtg.sh)
 }
 
-#16 jinwyp版一键
+#11 安装歇斯底里
+hysteria(){
+    bash <(curl -fsSL https://git.io/hysteria.sh)
+}
+
+# #11 安装naiveproxy
+# naive(){
+#         bash <(curl -s -L https://raw.githubusercontent.com/ChellyL/aio/main/naiveproxy.sh)
+# }
+
+# #12 安装naiveproxy yg原版
+# naive_yg(){
+#         wget -N https://gitlab.com/rwkgyg/naiveproxy-yg/raw/main/naiveproxy.sh && bash naiveproxy.sh
+# # }
+
+# #13 安装v2ray
+# V2Ray(){ 
+#     bash <(curl -s -L https://raw.githubusercontent.com/ChellyL/aio/main/v2.sh)
+# }
+
+#12 安装v2ray mack-a原版
+V2Ray_ma(){ 
+    wget -P /root -N --no-check-certificate "https://raw.githubusercontent.com/mack-a/v2ray-agent/master/install.sh" && chmod 700 /root/install.sh && /root/install.sh
+}
+
+
+#13 jinwyp版一键
 V2Ray_jin(){ 
     bash <(curl -Lso- https://git.io/oneclick)
     }
 
-#17 TrojanPanel 支持Xray/Trojan-Go/Hysteria/NaiveProxy的多用户Web管理面板
+#14 TrojanPanel 支持Xray/Trojan-Go/Hysteria/NaiveProxy的多用户Web管理面板
 TrojanPanel(){
       source <(curl -L https://github.com/trojanpanel/install-script/raw/main/install_script.sh)
+}
+
+#15 TrojanPanel 单机版 
+TrojanPanel_alone(){
+      source <(curl -L https://github.com/trojanpanel/install-script/raw/main/install_script_standalone.sh)
+
 }
 
 echo "+-------------------------------------------------------------+"
 echo "|                         自用小鸡脚本                        |"                                      
 echo "+-------------------------------------------------------------+"
-green "01.  新鸡fresh up"
-green "02.  更改时区CST"
-green "03.  bbr加速+一键dd"
-green "04.  ip归属及ipv46转换"
-green "05.  流媒体测试"
-green "06.  warp设置"
-green "07.  使用root登录[适合甲骨文、AZ等]"
-white "--------------------------------------------------------------"
-green "08.  SS"
-green "09.  acme证书管理"
-green "10.  歇斯底里"
-green "11.  naiveproxy-简化"
-green "12.  v2ray-简化"
-green '13.  Tg代理mtg'
-green "14.  jinwyp版一键  "
-green "15.  TrojanPanel"
+green "01. 新鸡fresh up"
+green "02. 更改时区CST"
+green "03. bbr加速+一键dd"
+green "04. ip归属及ipv46转换"
+green "05. 流媒体测试"
+green "06. warp设置"
+green "07. 使用root登录[适合甲骨文、AZ等]"
+green "08. acme证书管理"
+green '09. Tg代理mtg'
 white "---------------------------------------------------------------"
-green "16.  naiveproxy yg原版"
-green '17.  v2ray mack-a原版'
-red " 0.  退出"
+green "10. SS"
+green "11. Hysteria"
+green "12. mack-a版 v2ray"
+green "13. jinwyp版一键  "
+green "14. TrojanPanel 联机"
+green "15. TrojanPanel 单机"
+#green "11. naiveproxy-简化"
+#green "12. v2ray-简化"
+red " 0. 退出"
 blue "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 echo "证书路径："
 echo "hysteria：/root/.local/share/certmagic/certificates/acme-v02.api.letsencrypt.org-directory/"
-echo "naivepproxy：/root/ygkkkca"
+#echo "naivepproxy：/root/ygkkkca"
 echo "mack-a：/etc/v2ray-agent/tls"
 echo "jinway：/nginxweb/cert"
 echo "acme：/root/"
 echo "TrojanPanel: /tpdata/caddy/cert/"
 blue "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-read -p "请输入数字 [0-17]:" num
+read -p "请输入数字 [0-15]:" num
 case "$num" in
     1)
     updatevps
@@ -168,39 +175,39 @@ case "$num" in
     login
     ;;
     8)
-    ss
-    ;;
-    9)
     acme
     ;;
-    10)
-    hysteria
-    ;;
-    11)
-    naive
-    ;;
-    12)
-    V2Ray
-    ;;
-    13)
+    9)
     Tg_go
     ;;
-    14)
-    V2Ray_jin
+    10)
+    ss
     ;;
-    15)
-    TrojanPanel
+    11)
+    hysteria
     ;;
-    16)
-    naive_yg
-    ;;
-    17)
+    12)
     V2Ray_ma
     ;;
+    13)
+    V2Ray_jin
+    ;;
+    14)
+    TrojanPanel
+    ;;
+    15)
+    TrojanPanel_alone
+    ;;
+    # 16)
+    # naive_yg
+    # ;;
+    # 17)
+    # V2Ray_ma
+    # ;;
     0)
     exit
     ;;
     *)
-    echo "请输入正确数字 [0-17]"
+    echo "请输入正确数字 [0-15]"
     ;;
  esac
